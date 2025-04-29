4452100bf24a5ae7590a4d77a617ee2459059a97
Gabriel Riera <gabriel.riera@uib.es>
2025-04-29 10:41:52 +0200

    add docker tag to build-static.sh

--- build-static.sh
+++ build-static.sh
@@ -1,1 +1,1 @@
-podman run -v "$PWD:/volume" --rm -t -w /volume docker.io/clux/muslrust cargo build --release
+podman run -v "$PWD:/volume" --rm -t -w /volume docker.io/clux/muslrust:1.88.0-nightly cargo build --release
